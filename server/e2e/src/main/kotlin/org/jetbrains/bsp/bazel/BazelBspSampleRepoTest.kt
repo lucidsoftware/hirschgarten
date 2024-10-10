89d8ec9c45fad533ce6a5a33e59c479cc9ea3bfe
Ryan Ferguson <ryan28561@gmail.com>
2024-10-10 10:34:04 -0600

    revert fefd88c77853e7c71e1be5e5ca19e02e00521f47 so we get go to definition again

--- server/e2e/src/main/kotlin/org/jetbrains/bsp/bazel/BazelBspSampleRepoTest.kt
+++ server/e2e/src/main/kotlin/org/jetbrains/bsp/bazel/BazelBspSampleRepoTest.kt
@@ -586,7 +586,9 @@ object BazelBspSampleRepoTest : BazelBspTestBaseScenario() {
     val targetWithDependencyJavaBinary =
       DependencySourcesItem(
         BuildTargetIdentifier("$targetPrefix//target_with_dependency:java_binary"),
-        emptyList(),
+        listOf(
+          "file://\$BAZEL_OUTPUT_BASE_PATH/external/guava/guava-28.0-jre-src.jar",
+        ),
       )
     val javaTargetsSubpackageJavaLibrary =
       DependencySourcesItem(
